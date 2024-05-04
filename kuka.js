77be555028cadc5ad377b3796b354b5a2adc5955
Rami <ramon_volcom@mail.ru>
2024-05-04 17:25:03 +0300

    добавленны дз 29

--- kuka.js
+++ kuka.js
@@ -1,1 +1,462 @@
-gi
+'use strict';
+
+// // if (4 !== 9) {
+// //     console.log('OK!');
+// // } else {
+// //     console.log('Error!');
+// // };
+
+
+// // const num =50;
+// // if (num < 49) {
+// //     console.log('error');
+// // }else if (num > 100) {
+// //     console.log('mnogo');
+// // }else {
+// //     console.log('good');
+// // };
+// // (num === 111) ? console.log('OK!') : console.log('net!');
+
+// // const num =50;
+
+// // switch (num) {
+// //     case 49:
+// //         console.log('net');
+// //         break;
+// //     case 100:
+// //         console.log('net');
+// //         break;
+// //     case 50:
+// //         console.log('v tochku!');
+// //         break; 
+// //     default:
+// //         console.log('vse huin9|') 
+// //         break;      
+// // };
+
+
+// // const hamburger = true;
+// // const fries = true;
+
+// // if (hamburger && fries) {
+// //     console.log('я сыт!');
+// // }else if (hamburger && fries) {
+// //     console.log('я голоден');
+// // }else {
+// //     console.log('pizda');
+// // };
+
+// // console.log(hamburger && fries);
+
+// // const hamburger = 2;
+// // const fries = 1;
+// // const cola = 0;
+
+// // console.log(hamburger === 3 && cola && fries);
+
+// // if (hamburger === 3 && fries){
+// //     console.log('все сыты');
+// // }else {
+// //     console.log('идем дальше');
+// // };
+
+// // console.log(1 && 0);
+// // console.log(1 && 5);
+// // console.log(null && 5);
+// // console.log(0 && 'fsdgfsdfs');
+
+// // const hamburger = 0;
+// // const fries = 0;
+// // const cola = null;
+
+// // if (hamburger || fries || cola) {
+// //     console.log(' сыты');
+// // }else {
+// //     console.log('идем дальше');
+// // };
+// // console.log((hamburger || fries || cola))
+
+
+// // let jhonReport, alexReport, samReport, mariaReport ='done';
+// // console.log(jhonReport || alexReport || samReport || mariaReport)
+
+// // const hamburger = 3;
+// // const fries = 3;
+// // const cola = 0;
+// // const nuggets = 2;
+
+// // if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// //     console.log(' сыты');
+// // }else {
+// //     console.log('идем дальше');
+// // };
+// // console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets)
+
+
+// // console.log( NaN || 2 || undefined );
+
+
+// // console.log( NaN && 2 && undefined );
+
+
+// // console.log( 1 && 2 && 3 );
+
+
+// // console.log( !1 && 2 || !3 );
+
+
+// // console.log( 25 || null && !3 );
+
+
+// // console.log( NaN || null || !3 || undefined || 5);
+
+
+// // console.log( NaN || null && !3 && undefined || 5);
+
+
+// // console.log( 5 === 5 && 3 > 1 || 5);
+
+
+
+// // const hamburger = 3;
+// // const fries = 3;
+// // const cola = 0;
+// // const nuggets = 2;
+
+
+// // if (hamburger === 3 && cola || fries === 3 && nuggets) {
+// //    console.log('Done!')
+// // }
+
+
+// // let hamburger;
+// // const fries = NaN;
+// // const cola = 0;
+// // const nuggets = 2;
+
+
+// // if (hamburger || cola || fries === 3 || nuggets) {
+// //    console.log('Done!')
+// // }
+
+
+// // let hamburger;
+// // const fries = NaN;
+// // const cola = 0;
+// // const nuggets = 2;
+
+
+// // if (hamburger && cola || fries === 3 && nuggets) {
+// //    console.log('Done!')
+// // }
+
+
+// // let num =50;
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++
+// // }
+
+// // let num = 50;
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num < 55);
+
+
+// // let num = 50;
+
+// // for (let i = 1; i < 20; i++) {
+// //     if (num === 57){
+// //         // break;
+// //         continue;
+// //     };
+
+// //     console.log(num);
+// //     num++;
+// // };
+
+// // for (let i = 1; i < 20; i++) {
+// //     if (i === 10) {
+// //         continue;
+// //     };
+
+// //     console.log(i);
+// // };
+
+// // for (let i = 0; i < 3; i++) {
+// //     console.log(i);
+// //     for (let j = 0; j < 3; j++) {
+// //         console.log(j);
+// //         for (let k = 0; k < 3; k++){
+// //             console.log(k);
+// //         };
+// //     };
+// // };
+
+// // *
+// // **
+// // ***
+// // ****
+// // *****
+// // ******
+
+// // let result = '';
+// // const length = 7;
+
+
+// // for (let i = 1; i < length; i++){
+
+// //     for(let j  =0; j < i; j++){
+// //             result +='*'
+// //     }
+
+// //     result +='\n';
+
+// // };
+// // console.log(result)
+
+
+// // first: for (let i = 0; i < 3; i++) {
+// //         console.log(`first level ${i}`);
+// //         for (let j = 0; j < 3; j++) {
+// //             console.log(`second level ${j}`);
+// //             for (let k = 0; k < 3; k++){
+// //                 if (k === 2) break first;
+// //                 console.log(`therd level ${k}`);
+// //             };
+// //         };
+// //     };
+
+
+//     // Пишем решение вот тут
+//     // for (let i = 2; i <=10; i++){
+//     //     if (i % 2 === 0){
+//     //         console.log(i);
+//     //     }
+//     // };
+
+
+
+
+
+    
+//     // for (let i = 2; i <= 16; i++) {
+//     //         if (i % 2 === 0) {
+//     //             continue;
+//     //         } else {
+//     //             console.log(i);
+//     //         }
+//     //     }
+
+
+//         // let num =2
+//         // while (num < 16) {
+//         //     if (num % 2 ===0){
+//         //         continue;
+//         //         else { 
+//         //             console.log(num);
+//         //             }
+//         //     } 
+           
+//         // };
+
+//         // let i =2
+//         // while (i <=15) {
+//         //     if (i % 2 ===1){
+//         //         console.log(i);
+//         //     }
+            
+//         //     i++;
+//         // }
+
+//         // // Место для первой задачи
+//         // function firstTask() {
+//         //     const arr = [3, 5, 8, 16, 20, 23, 50];
+//         //     const result = [];
+        
+//         //     for (let i = 0; i < arr.length; i++) {
+//         //         result[i] = arr[i];
+//         //     }
+        
+//         //     console.log(result);
+//         //     return result;
+//         // }
+
+//         // for (let i = 2; i <= 16; i++) {
+//         //         if (i % 2 === 0) {
+//         //             continue;
+//         //         } else {
+//         //             console.log(i);
+//         //         }
+//         //     }
+
+//         //     let i =2
+//         //     while (i <= 16) {
+//         //         if (i % 2 === 1) 
+//         //             console.log(i);
+//         //         i++
+//         //     }
+                
+//         //         let i=2
+//         //         while (i <=16){
+//         //             if (i % 2 === 0){
+//         //                 i++
+//         //                 continue;
+//         //             }else {
+//         //                 console.log(i);
+//         //             }
+//         //             i++
+//         //         }
+            
+
+//         // let i = 2;
+
+//         // while (i <= 16) {
+//         //     if (i % 2 === 0) {
+//         //         i++;
+//         //         continue;
+//         //     } else {
+//         //         console.log(i);
+//         //     }
+//         //     i++;
+//         // }
+
+//         // function fifthTask() {
+//         //     const arrayOfNumbers = [];
+        
+//         //     for (let i = 5; i < 11; i++) {
+//         //         arrayOfNumbers[i - 5] = i;
+//         //     }
+        
+//         //     console.log(arrayOfNumbers);
+//         //     return arrayOfNumbers;
+//         // }
+
+
+//         // for (let i = 5; i <= 10; i++){
+//         //     arrayOfNumbers[i - 5] = i;
+//         // }
+//         // console.log(arrayOfNumbers)
+
+
+
+
+
+
+
+
+
+//         function fifthTask() {
+//             const arrayOfNumbers = [];
+        
+//             // Пишем решение вот тут
+//             for (i = 5; i <= 10; i++){
+//                 arrayOfNumbers[i - 5] = i;
+//             }
+//             console.log(arrayOfNumbers)
+            
+//             for (let i = 5; i < 11; i++) {
+//                 arrayOfNumbers[i - 5] = i;
+//             }
+        
+//             console.log(arrayOfNumbers);
+//             // Не трогаем
+//             return arrayOfNumbers;
+//         }
+
+
+// let num = 20;
+// function showFirstMessage(text) {
+//     console.log(text); 
+    
+//     console.log(num);
+// }
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+
+// function ret(){
+//     let num =50;
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// let ret = function(){
+//     let num =50;
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+
+// const calcc = (a, b) => {
+//     console.log('3333');
+//     return (a+b)};
+// console.log(calcc(4, 5));
+// calcc()
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return(curr * amount);
+// }
+
+// function promotion(result) {
+//     console.log(result * discount)
+// }
+
+// promotion(convert (100, usdCurr))
+
+
+// function test (){
+//     for (let i = 0; i < 5; i++){
+//         console.log(i);
+//         if (i === 3) return
+//     }
+//     console.log('done');
+// };
+
+// test();
+
+
+
+
+// function sayHello(name) {
+// return (`Привет, ${name}!`)
+// }
+// sayHello('Антон')
+
+// function getMathResult(num, times) {
+//     if (typeof(times) !== 'number' || times <= 0) {
+//         return num;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= times; i++) {
+//         if (i === times) {
+//             str += `${num * i}`;
+//             // Тут без черточек в конце
+//         } else {
+//             str += `${num * i}---`;
+//             // Это тоже самое, что и
+//             // str = str + num * i + "---"
+//         }
+//     }
+
+//     return str;
+// }
+
+// getMathResult(10, 5);
+
+
+const logg = 'Hello world';
+console.log(logg.slice (6, 11));
+console.log(logg.slice (6));
+console.log(logg.slice(-5))
